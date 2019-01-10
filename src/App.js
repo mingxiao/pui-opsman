da86de2f7578ae011ce0738865b4053e3100446c
Ming Xiao <mxiao@pivotal.io>
2019-01-09 17:10:24 -0800

    add changelog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 import {Siteframe} from 'pivotal-ui/react/siteframe';
 import {Icon} from 'pivotal-ui/react/iconography';
 import {Grid, FlexCol} from 'pivotal-ui/react/flex-grids';
 import {PrimaryButton, DefaultButton} from 'pivotal-ui/react/buttons';
 import 'pivotal-ui/css/colors';
+import ChangeLog from './changelog';
 
 class App extends Component {
   render() {
@@ -12,21 +13,27 @@ class App extends Component {
       <div className="App">
         <Siteframe {...{
           headerProps: {
-            logo: <div className="ptl pbl pll" style={{fontSize: '40px'}}><Icon src="pivotal_p" style={{fill: 'currentColor'}}/></div>,
+            logo: <div className="ptl pbl pll" style={{fontSize: '40px'}}><Icon src="pivotal_p"
+                                                                                style={{fill: 'currentColor'}}/></div>,
             companyName: 'PCF',
             productName: 'Ops Manager'
           },
         }}>
-          <Grid className="page">
-            <FlexCol className="paxxl bg-neutral-8" fixed><PrimaryButton>Import a Product</PrimaryButton></FlexCol>
-            <FlexCol>
-              <Grid justifyContent="space-between">
-                <FlexCol fixed><div className="h2">Installation Dashboard</div></FlexCol>
-                <FlexCol fixed><DefaultButton flat icon={<Icon src="refresh"/>}>Revert</DefaultButton></FlexCol>
-              </Grid>
-            </FlexCol>
-            <FlexCol fixed><PrimaryButton>Review Pending Changes</PrimaryButton></FlexCol>
-          </Grid>
+          <Fragment>
+            <Grid className="page">
+              <FlexCol className="paxxl bg-neutral-8" fixed><PrimaryButton>Import a Product</PrimaryButton></FlexCol>
+              <FlexCol>
+                <Grid justifyContent="space-between">
+                  <FlexCol fixed>
+                    <div className="h2">Installation Dashboard</div>
+                  </FlexCol>
+                  <FlexCol fixed><DefaultButton flat icon={<Icon src="refresh"/>}>Revert</DefaultButton></FlexCol>
+                </Grid>
+              </FlexCol>
+              <FlexCol fixed><PrimaryButton>Review Pending Changes</PrimaryButton></FlexCol>
+            </Grid>
+            <ChangeLog/>
+          </Fragment>
         </Siteframe>
       </div>
     );
---
+++ src/changelog.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {AdvancedTable} from 'pivotal-ui/react/table';
+
+export default class ChangeLog extends React.Component {
+  render() {
+    const columns = [
+      {displayName: 'Started', attribute: 'started_at', width: '10%'},
+      {displayName: 'Finished', attribute: 'finished_at', width: '10%'},
+      {displayName: 'User', attribute: 'user_name', width: '9%'},
+      {
+        displayName: 'Result',
+        width: '10%',
+        className: 'installation-result',
+        renderTdChildren: ({status}) => {
+          const backgroundColor = status === 'failed' ? 'red' : 'green';
+          return <div style={{backgroundColor}}>{status}</div>;
+        }
+      },
+      {displayName: 'Added', attribute: 'additions', width: '14%'},
+      {displayName: 'Updated', attribute: 'updates', width: '14%'},
+      {displayName: 'Deleted', attribute: 'deletions', width: '14%'},
+      {displayName: 'Unchanged', attribute: 'unchanged', width: '14%'},
+      {displayName: 'Log', attribute: 'id', width: '5%'},
+    ];
+
+    const data = JSON.parse('[{"started_at":"2019-01-09 19:00:41 UTC","finished_at":"2019-01-09 19:07:12 UTC","user_name":"admin","status":"success","additions":"Ops Manager: Example Product service-broker 1.0-build.0\\nOps Manager: Example Product 2.5-committed.20181218.145035-stemcell.170.0","updates":"","deletions":"","unchanged":"BOSH Director 2.5-build.75"},{"started_at":"2019-01-09 18:35:55 UTC","finished_at":"2019-01-09 18:57:29 UTC","user_name":"admin","status":"failed","additions":"BOSH Director 2.5-build.75\\nOps Manager: Example Product vmtypes 1.0-build.0\\nOps Manager: Example Product service-broker 1.0-build.0\\nOps Manager: Example Product 2.5-committed.20181218.145035-stemcell.170.0","updates":"","deletions":"","unchanged":""}]');
+
+    console.log(data)
+    return (
+      <AdvancedTable columns={columns} data={data}/>
+    );
+  }
+}
